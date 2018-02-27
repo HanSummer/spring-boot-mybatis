3169ede7af582c43579a43d1ec111e7f38a3892f
HanSummer <xiahl0916@163.com>
2018-02-27 18:16:01 +0800

    commit init

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/8/16.
+ */
+public interface UserService {
+
+    int addUser(User user);
+
+    List<User> findAllUser(int pageNum, int pageSize);
+}
